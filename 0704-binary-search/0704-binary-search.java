7cc65941a0506778267f4227fb9a3c42d06da511
Akhil Reddy <94733954+AkhilBuchala@users.noreply.github.com>
2024-03-27 18:26:57 -0400

    Time: 0 ms (100.00%), Space: 45.6 MB (45.58%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,8 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-       int low =0;
-       int high = nums.length-1;
-       while(low<=high){
+        int low =0;
+        int high = nums.length-1;
+        while(low<=high){
            int mid = low + (high-low)/2;
            if(nums[mid]==target){
                return mid;
@@ -14,6 +14,6 @@ class Solution {
                low=mid+1;
            }
        }
-   return -1;
+        return -1;
     }
 }
